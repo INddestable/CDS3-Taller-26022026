9525c1d053df55d26d03245312da69926dbd42e0
INddestable <kevinkarv77708@gmail.com>
2026-02-26 21:00:39 -0500

    funcionamiento de division de tareas

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#agregar_tarea");
+const input = document.querySelector("#tareaInput");
+const divIncompletos = document.querySelector("#incompletos");
+const divCompletado = document.querySelector("#completado");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); //no recarga pg
+
+    const texto = input.value.trim();
+    if (texto === "") return;
+
+    const tareaDiv = document.createElement("div");
+    tareaDiv.textContent = texto;
+
+    let completado = false;
+
+    tareaDiv.addEventListener("click", function() {
+        completado = !completado;
+
+        if (completado) {
+            divCompletado.appendChild(tareaDiv);
+        } else {
+            divIncompletos.appendChild(tareaDiv);
+        }
+    });
+
+    divIncompletos.appendChild(tareaDiv);
+    input.value = "";
+});
